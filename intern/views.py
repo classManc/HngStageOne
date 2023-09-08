afe6cb412515afcb27ba7c0523caa045a30a0a52
Joseph <josephajakaye924@gmail.com>
2023-09-08 11:56:47 +0100

    Added random timedelta to handle the -2/+2 utc

--- intern/views.py
+++ intern/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime
 from django.utils import timezone
 from datetime import timedelta
+import random
 
 # Create your views here.
 
@@ -14,12 +16,12 @@ class InternDetails(APIView):
         track = request.query_params.get('track','None')
         current_datetime_utc = timezone.now()
         current_day = current_datetime_utc.strftime('%A')
-        timedelta_to_add = timedelta(minutes=2)
-        current_datetime_utc += timedelta_to_add
-        current_utc_time = current_datetime_utc.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-4] + 'Z'
-        github_file_url = 'https://github.com/classManc/HngStageOne.git/intern/views.py'
+        random_timedelta = timedelta(minutes=random.randint(-2, 2))
+        current_datetime_utc += random_timedelta
+        current_utc_time = current_datetime_utc.strftime('%Y-%m-%dT%H:%M:%S')+ 'Z'
+        github_file_url = 'https://github.com/classManc/HngStageOne/tree/main/intern/views.py'
         github_repo_url = 'https://github.com/classManc/HngStageOne.git'
 
         return Response({'slack_name':slack_name, 'track':track, 'current_day':current_day,
                          'utc_time':current_utc_time, 'github_file_url':github_file_url,
-                         'github_repo_url':github_repo_url, 'status':200})
+                         'github_repo_url':github_repo_url, 'status_code':200})
